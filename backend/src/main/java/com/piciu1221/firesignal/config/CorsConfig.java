ecee1b9a01465678c0a8557571252368411fa831
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2023-10-05 23:54:19 +0200

    Update CorsConfig.java

--- backend/src/main/java/com/piciu1221/firesignal/config/CorsConfig.java
+++ backend/src/main/java/com/piciu1221/firesignal/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://piciu1221.github.io") // Replace with the frontend URL
+                .allowedOrigins("*") // Replace with the frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
